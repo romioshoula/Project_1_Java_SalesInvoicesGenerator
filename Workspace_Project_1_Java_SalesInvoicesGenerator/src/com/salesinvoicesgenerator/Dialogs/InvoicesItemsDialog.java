12967b258f19cd532a90f63b83ebbd50ab5f5d70
Rami Wail Shoula <romioshoula@gmail.com>
2022-05-14 17:50:50 +0200

    Updated Dialogs Classes

--- Workspace_Project_1_Java_SalesInvoicesGenerator/src/com/salesinvoicesgenerator/Dialogs/InvoicesItemsDialog.java
+++ Workspace_Project_1_Java_SalesInvoicesGenerator/src/com/salesinvoicesgenerator/Dialogs/InvoicesItemsDialog.java
@@ -1,4 +1,66 @@
 package com.salesinvoicesgenerator.Dialogs;
 
-public class InvoicesItemsDialog {
+import com.salesinvoicesgenerator.JFrame.SalesInvoicesFrame;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+// This class represents the Invoices Items Dialog once pressed on Create New Line button.
+
+public class InvoicesItemsDialog extends JDialog {
+    // Variables declaration.
+    private JTextField itemNameJTextField;
+    private JTextField itemCountJTextField;
+    private JTextField itemPriceJTextField;
+    private JLabel itemNameLbl;
+    private JLabel itemCountLbl;
+    private JLabel itemPriceLbl;
+    private JButton okBtn;
+    private JButton cancelBtn;
+    // End of variables declaration.
+
+    // Invoice Items Dialog Constructor.
+    public InvoicesItemsDialog(SalesInvoicesFrame f) {
+        itemNameJTextField = new JTextField(20);
+        itemNameLbl = new JLabel("Item Name:");
+        itemCountJTextField = new JTextField(20);
+        itemCountLbl = new JLabel("Item Count:");
+        itemPriceJTextField = new JTextField(20);
+        itemPriceLbl = new JLabel("Item Price:");
+        okBtn = new JButton("OK");
+        cancelBtn = new JButton("Cancel");
+        okBtn.setActionCommand("confirmCreateLine");
+        cancelBtn.setActionCommand("cancelCreateLine");
+        okBtn.addActionListener(f);
+        cancelBtn.addActionListener(f);
+        setLayout(new GridLayout(4,2));
+        add(itemNameLbl);
+        add(itemNameJTextField);
+        add(itemCountLbl);
+        add(itemCountJTextField);
+        add(itemPriceLbl);
+        add(itemPriceJTextField);
+        add(okBtn);
+        add(cancelBtn);
+        pack();
+    }
+
+    // Item Name Text Field Getter.
+    public JTextField getItemNameJTextField() {
+        return itemNameJTextField;
+    }
+
+    // Item Count Text Field Getter.
+    public JTextField getItemCountJTextField() {
+        return itemCountJTextField;
+    }
+
+    // Item Price Text Field Getter.
+    public JTextField getItemPriceJTextField() {
+        return itemPriceJTextField;
+    }
+
 }
+
--- Workspace_Project_1_Java_SalesInvoicesGenerator/src/com/salesinvoicesgenerator/Dialogs/InvoicesTableDialog.java
+++ Workspace_Project_1_Java_SalesInvoicesGenerator/src/com/salesinvoicesgenerator/Dialogs/InvoicesTableDialog.java
@@ -1,4 +1,63 @@
 package com.salesinvoicesgenerator.Dialogs;
 
-public class InvoicesTableDialog {
+
+// This class represents the Invoices Table Dialog once pressed on Create New Invoice button.
+
+
+import com.salesinvoicesgenerator.JFrame.SalesInvoicesFrame;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+
+public class InvoicesTableDialog extends JDialog {
+    // Variables Declaration.
+    private JTextField customerNameField;
+    private JTextField invoiceDateField;
+    private JLabel customerNameJLabel;
+    private JLabel invoiceDateJLabel;
+    private JButton okBtn;
+    private JButton cancelBtn;
+    // End of Variables Declaration.
+
+    // Constructor.
+    public InvoicesTableDialog(SalesInvoicesFrame f) {
+        customerNameJLabel = new JLabel("Customer Name:");
+        customerNameField = new JTextField(20);
+        invoiceDateJLabel = new JLabel("Invoice Date:");
+        invoiceDateField = new JTextField(20);
+        okBtn = new JButton("OK");
+        cancelBtn = new JButton("Cancel");
+
+// OK & Cancel buttons action commands.
+        okBtn.setActionCommand("confirmCreateInvoice");
+        cancelBtn.setActionCommand("cancelCreateInvoice");
+        okBtn.addActionListener(f);
+        cancelBtn.addActionListener(f);
+
+// Setting Layout.
+        setLayout(new GridLayout(4, 2));
+
+// Adding declared constructed variables to Layout.
+        add(customerNameJLabel);
+        add(customerNameField);
+        add(invoiceDateJLabel);
+        add(invoiceDateField);
+        add(okBtn);
+        add(cancelBtn);
+        pack();
+    }
+
+    // Getter Method for Customer Name.
+    public JTextField getCustomerNameField() {
+        return customerNameField;
+    }
+
+    // Getter Method for Invoice Date.
+    public JTextField getInvoiceDateField() {
+        return invoiceDateField;
+    }
+
 }
