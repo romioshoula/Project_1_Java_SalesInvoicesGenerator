04306af6a826a209f1ff0133872a155351e3dc4c
Rami Wail Shoula <romioshoula@gmail.com>
2022-05-14 17:52:33 +0200

    Updated AppMain Class

--- Workspace_Project_1_Java_SalesInvoicesGenerator/src/com/salesinvoicesgenerator/Main/AppMain.java
+++ Workspace_Project_1_Java_SalesInvoicesGenerator/src/com/salesinvoicesgenerator/Main/AppMain.java
@@ -1,4 +1,15 @@
 package com.salesinvoicesgenerator.Main;
 
+// This class defines the App Main Class.
+
+import com.salesinvoicesgenerator.JFrame.SalesInvoicesFrame;
+
+
 public class AppMain {
+    public static void main(String[] args) {
+        SalesInvoicesFrame f = new SalesInvoicesFrame();
+        f.setVisible(true);
+    }
+
 }
+
